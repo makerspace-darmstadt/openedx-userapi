fa6eb4e864e7f0ee0b3631496f8cc66dd6bd5b70
Daniel Herrmann <daniel.herrmann1@gmail.com>
2023-09-01 19:08:18 +0200

    Remove unneccessary endpoints, switch to new Django URL format

--- openedx_userapi/urls.py
+++ openedx_userapi/urls.py
@@ -1,69 +1,35 @@
 """
 URLs for openedx_userapi.
 """
-from django.urls import patterns, url
+from django.urls import re_path
 
 from openedx_userapi import views
 
-urlpatterns = patterns(
-    "",
-    # user API
-    url(
+
+urlpatterns = [
+    re_path(
         r"^accounts/user_without_password",
         views.CreateUserAccountWithoutPasswordView.as_view(),
         name="create_user_account_without_password_api",
     ),
-    url(
+    re_path(
         r"^accounts/create",
         views.CreateUserAccountView.as_view(),
         name="create_user_account_api",
     ),
-    url(
+    re_path(
         r"^accounts/connect",
         views.UserAccountConnect.as_view(),
         name="user_account_connect_api",
     ),
-    url(
+    re_path(
         r"^accounts/update_user",
         views.UpdateUserAccount.as_view(),
         name="user_account_update_user",
     ),
-    url(
+    re_path(
         r"^accounts/get-user/(?P<username>[\w.+-]+)",
         views.GetUserAccountView.as_view(),
         name="get_user_account_api",
     ),
-    # bulk enrollment API
-    url(
-        r"^bulk-enrollment/bulk-enroll",
-        views.BulkEnrollView.as_view(),
-        name="bulk_enrollment_api",
-    ),
-    # enrollment codes API
-    url(
-        r"^enrollment-codes/generate",
-        views.GenerateRegistrationCodesView.as_view(),
-        name="generate_registration_codes_api",
-    ),
-    url(
-        r"^enrollment-codes/enroll-user",
-        views.EnrollUserWithEnrollmentCodeView.as_view(),
-        name="enroll_use_with_code_api",
-    ),
-    url(
-        r"^enrollment-codes/status",
-        views.EnrollmentCodeStatusView.as_view(),
-        name="enrollment_code_status_api",
-    ),
-    # enrollment analytics API
-    url(
-        r"^analytics/accounts/batch",
-        views.GetBatchUserDataView.as_view(),
-        name="get_batch_user_data",
-    ),
-    url(
-        r"^analytics/enrollment/batch",
-        views.GetBatchEnrollmentDataView.as_view(),
-        name="get_batch_enrollment_data",
-    ),
-)
+]
